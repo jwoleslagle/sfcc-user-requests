dd91bc84ddfe1c9460dffa3aae50bd641d3ee620
Jeff Woleslagle <jwoleslagle@gmail.com>
2020-03-05 17:57:25 -0500

    Working on bash script

--- ecs/userAccessCmds.sh
+++ ecs/userAccessCmds.sh
@@ -1,17 +1,38 @@
 #!/bin/sh
-# a shell script used to download a specific url.
-DIR=/cygdrive/c/Al/Reports
 
-# wget output file
-FILE=.`date +"%Y%m%d"`
-# date --iso-8601=seconds
+ADDFILE=.ADD_`date --iso-8601=seconds`.json
+DELFILE=.DEL_`date --iso-8601=seconds`.json
+# date +"%Y%m%d"
 
-# wget log file
 LOGFILE=wget.log
 
 # wget download url
-URL=http://foo.com/myurl.html
+ADDURL=https://op857sfym8.execute-api.us-east-1.amazonaws.com/beta/rqsts/add
+DELURL=https://op857sfym8.execute-api.us-east-1.amazonaws.com/beta/rqsts/delete
 
-cd $DIR
-wget $URL -O $FILE -o $LOGFILE
+wget $ADDURL -O $ADDFILE -o $LOGFILE
+toAddObj=$(<$ADDFILE)
 
+wget $DELURL -O $DELFILE -o $LOGFILE
+toDeleteObj=$(<$DELFILE)
+
+ADD_COUNT=`jq '.[] | .Count' $ADDFILE`
+DELETE_COUNT=`jq '.[] | .Count' $DELFILE`
+
+ADD_ARRAY=`jq '.[] | .Items' $ADDFILE`
+DEL_ARRAY=`jq '.[] | .Items' $DELFILE`
+
+#Authorize the client
+`sfcc-ci client:auth 9ea5c0b3-3dad-4338-a762-c7d760ec7d88 hbc2020!`
+
+# for i in "${arrayName[@]}"
+# do
+#   : 
+#   # do whatever on $i
+# done
+
+# print_something () {
+#     echo Hello $1
+# }
+# print_something Mars
+# print_something Jupiter
